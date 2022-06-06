e9532c1fafbbbbe2a591feb23927e5746a344b99
LoQeN00 <LoQeN00@o2.pl>
2022-06-06 14:13:26 +0200

    First commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  compress: true,
+  optimizeFonts: true,
+  experimental: {
+    gzipSize: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
