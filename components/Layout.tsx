9f8afa282673b8b17c919449b72a0a80cac00177
LoQeN00 <LoQeN00@o2.pl>
2022-07-07 18:26:34 +0200

    markdowns

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -11,29 +11,6 @@ type Props = {
 export const Layout = ({ children }: Props) => {
   return (
     <div className="flex flex-col min-h-screen bg-teal-100">
-      <NextSeo
-        openGraph={{
-          url: 'https://www.url.ie/a',
-          images: [
-            {
-              url: 'https://www.example.ie/og-image-01.jpg',
-              width: 800,
-              height: 600,
-              alt: 'Og Image Alt',
-              type: 'image/jpeg',
-            },
-            {
-              url: 'https://www.example.ie/og-image-02.jpg',
-              width: 900,
-              height: 800,
-              alt: 'Og Image Alt Second',
-              type: 'image/jpeg',
-            },
-            { url: 'https://www.example.ie/og-image-03.jpg' },
-            { url: 'https://www.example.ie/og-image-04.jpg' },
-          ],
-        }}
-      />
       <Header />
       <Main>{children}</Main>
       <Footer />
