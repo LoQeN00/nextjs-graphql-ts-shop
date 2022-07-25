10df28af83d1ae163801f58d13af773f4bf6f0d1
LoQeN00 <LoQeN00@o2.pl>
2022-07-25 19:32:42 +0200

    Fixed checkout

--- components/Cart/CartSummary.tsx
+++ components/Cart/CartSummary.tsx
@@ -15,12 +15,16 @@ const CartSummary = (props: Props) => {
     <div>
       <div className="font-bold">Element count: {items.length}</div>
       <div className="font-bold">Price: {fullPrice}</div>
-      <button onClick={clearCart} className="px-4 py-2 border-2 border-black rounded-2xl">
-        Clear cart
-      </button>
-      <Link href="/checkout">
-        <a className="px-4 py-2 border-2 border-black rounded-2xl ml-5 inline-block">Go to checkout</a>
-      </Link>
+      <div className="flex flex-col sm:flex-row w-full">
+        <button onClick={clearCart} className="px-4 py-2 border-2 border-black rounded-2xl">
+          Clear cart
+        </button>
+        {items.length > 0 && (
+          <Link href="/checkout">
+            <a className="px-4 py-2 border-2 border-black rounded-2xl ml-5 inline-block">Go to checkout</a>
+          </Link>
+        )}
+      </div>
     </div>
   );
 };
