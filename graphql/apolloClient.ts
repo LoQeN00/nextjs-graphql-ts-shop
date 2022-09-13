259b9c1bd072def4f0a1ddac0ae90f9e44073e50
LoQeN00 <LoQeN00@o2.pl>
2022-09-13 15:47:54 +0200

    Added name and surname to session

--- graphql/apolloClient.ts
+++ graphql/apolloClient.ts
@@ -3,16 +3,16 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 export const client = new ApolloClient({
   uri: 'https://api-us-west-2.hygraph.com/v2/cl5me6ad9524n01uofjrw3hi2/master',
   cache: new InMemoryCache(),
-  defaultOptions: {
-    watchQuery: {
-      nextFetchPolicy(currentFetchPolicy) {
-        if (currentFetchPolicy === 'network-only' || currentFetchPolicy === 'cache-and-network') {
-          return 'cache-first';
-        }
-        return currentFetchPolicy;
-      },
-    },
-  },
+  // defaultOptions: {
+  //   watchQuery: {
+  //     nextFetchPolicy(currentFetchPolicy) {
+  //       if (currentFetchPolicy === 'network-only' || currentFetchPolicy === 'cache-and-network') {
+  //         return 'cache-first';
+  //       }
+  //       return currentFetchPolicy;
+  //     },
+  //   },
+  // },
 });
 
 export const authorizedClient = new ApolloClient({
