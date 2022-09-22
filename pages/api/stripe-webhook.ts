bb885edf079c93df7bec1dec2ada2a1f46a2aa0c
LoQeN00 <LoQeN00@o2.pl>
2022-09-22 14:09:22 +0200

    Added webhook signature veryfying

--- pages/api/stripe-webhook.ts
+++ pages/api/stripe-webhook.ts
@@ -8,11 +8,48 @@ import {
   FindAllAccountOrdersDocument,
 } from './../../generated/graphql';
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiHandler } from 'next';
+import type { NextApiHandler, PageConfig } from 'next';
 import { authorizedClient } from '../../graphql/apolloClient';
+import { Readable } from 'stream';
+import { Stripe } from 'stripe';
+
+async function buffer(readable: Readable) {
+  const chunks = [];
+  for await (const chunk of readable) {
+    chunks.push(typeof chunk === 'string' ? Buffer.from(chunk) : chunk);
+  }
+  return Buffer.concat(chunks);
+}
 
 const handler: NextApiHandler = async (req, res) => {
-  const event = req.body;
+  const body = await buffer(req);
+  const signature = req.headers['stripe-signature'];
+  const stripeKey = process.env.STRIPE_SECRET_KEY;
+
+  console.log(body);
+
+  if (!stripeKey) {
+    res.status(500).json({ message: 'MISSING STRIPE KEY' });
+    return;
+  }
+
+  if (typeof signature !== 'string') {
+    res.status(500).json({ message: 'SIGNATURE IS NOT A STRING' });
+    return;
+  }
+
+  if (!process.env.STRIPE_WEBHOOK_SECRET) {
+    res.status(500).json({ message: 'MISSING STRIPE WEBHOOK SECRET' });
+    return;
+  }
+
+  const stripe = new Stripe(stripeKey, { apiVersion: '2022-08-01' });
+
+  const event = stripe.webhooks.constructEvent(body, signature, process.env.STRIPE_WEBHOOK_SECRET);
+
+  console.log(event);
+
+  // const event = req.body;
 
   const changeOrderStatus = async (id: string) => {
     const order = await authorizedClient.mutate<
@@ -25,14 +62,6 @@ const handler: NextApiHandler = async (req, res) => {
         state: 'PAID',
       },
     });
-
-    // const publishOrder = await authorizedClient.mutate<PublishOrderMutation, PublishOrderMutationVariables>({
-    //   mutation: PublishOrderDocument,
-    //   variables: {
-    //     id: order.data?.updateOrder?.id!,
-    //   },
-    //   fetchPolicy: 'network-only',
-    // });
   };
 
   switch (event.type) {
@@ -42,6 +71,7 @@ const handler: NextApiHandler = async (req, res) => {
       break;
 
     case 'checkout.session.completed':
+      //@ts-ignore
       changeOrderStatus(event.data.object.id);
       break;
   }
@@ -50,3 +80,9 @@ const handler: NextApiHandler = async (req, res) => {
 };
 
 export default handler;
+
+export const config: PageConfig = {
+  api: {
+    bodyParser: false,
+  },
+};
